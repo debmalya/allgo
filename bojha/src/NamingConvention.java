7cb5ded4ef772bf2dd183eb11bdd2552ae7a5eca
Debmalya Jash <debmalya.jash@gmail.com>
2014-11-15 00:14:36 -0800

    initial commit

---
+++ bojha/src/NamingConvention.java
@@ -0,0 +1,26 @@
+public class NamingConvention {
+
+	/*
+	 * Make snake case variable name to camel case.
+	 */
+	public String toCamelCase(String variableName) {
+		StringBuilder camelCase = new StringBuilder();
+
+		int len = variableName.length();
+		for (int i = 0; i < len; i++) {
+
+			char ch = variableName.charAt(i);
+			if (ch == '_') {
+				if (i < len - 1) {
+					ch = variableName.charAt(i + 1);
+					ch = (char) (ch + 26);
+				}
+			}
+			camelCase.append(ch);
+
+		}
+
+		return camelCase.toString();
+	}
+
+}
