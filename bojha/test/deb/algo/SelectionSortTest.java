e2ce9b052c4cb80d5d4b0573d3b3b0ddaf81cc56
Debmalya Jash <debmalya.jash@gmail.com>
2015-02-16 21:45:21 +0800

    Modification

---
+++ bojha/test/deb/algo/SelectionSortTest.java
@@ -0,0 +1,15 @@
+package deb.algo;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class SelectionSortTest {
+
+	@Test
+	public void testSort() {
+		Date date = new Date(System.currentTimeMillis());
+		
+	}
+
+}
