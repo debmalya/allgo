e70226bdba79892c8cee6872c5c6472d48c496a1
Debmalya Jash <debmalya.jash@gmail.com>
2015-02-24 16:09:01 +0800

    Initial commit

---
+++ bojha/src/deb/algo/InsertionSort.java
@@ -0,0 +1,26 @@
+package deb.algo;
+
+public class InsertionSort
+{
+
+  public Comparable[] sort (Comparable[] values)
+  {
+    for (int i = 1; i < values.length; i++)
+    {
+
+      for (int j = i; j > 0; j--)
+      {
+
+        if (values[j - 1].compareTo (values[j]) > 0)
+        {
+
+          Comparable swap = values[j];
+          values[j] = values[j - 1];
+          values[j - 1] = swap;
+
+        }
+      }
+    }
+    return values;
+  }
+}
