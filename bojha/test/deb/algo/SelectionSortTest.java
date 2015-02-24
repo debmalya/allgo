45491db4c58ffd879b718be738a829d954d8a68f
Debmalya Jash <debmalya.jash@gmail.com>
2015-02-24 22:30:38 +0800

    intial commit

--- bojha/test/deb/algo/SelectionSortTest.java
+++ bojha/test/deb/algo/SelectionSortTest.java
@@ -1,15 +1,27 @@
 package deb.algo;
 
-import java.util.Date;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class SelectionSortTest {
 
 	@Test
 	public void testSort() {
-		Date date = new Date(System.currentTimeMillis());
+		Integer[] values = new Integer[10];
 		
+		for (int i = 0; i < 10; i++) {
+			values[i] = (int)(Math.random()*1000);
+		}
+		
+		SelectionSort sort = new SelectionSort();
+		values = (Integer[])sort.sort(values);
+		
+		for (int i = 0; i < 10;i++) {
+			System.out.println(values[i]);
+			if (i > 0) {
+				Assert.assertTrue(values[i- 1] <= values[i]);
+			}
+		}
 	}
 
 }
