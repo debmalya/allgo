5eee121758e345a37f1127cc1615e123d4cbbca5
Debmalya Jash <debmalya.jash@gmail.com>
2015-02-25 10:02:39 +0800

    Modified added time taken for sorting

--- bojha/test/deb/algo/InsertionSortTest.java
+++ bojha/test/deb/algo/InsertionSortTest.java
@@ -5,19 +5,22 @@ import org.junit.Test;
 public class InsertionSortTest {
 	@Test
 	public void testSort() {
-	  int length = 10;
+	  long startTime = System.currentTimeMillis ();
+	  int length = 10000;
 		Integer[] values = new Integer[length];
 		
 		for (int i = 0;i < length;i ++) {
 		    values[i] = (int)(Math.random()*100);
 		}
 		
-		printValues(values,"Before sort");
+//		printValues(values,"Before sort");
 		
 		InsertionSort insertionSort = new InsertionSort ();
 		values = (Integer[])insertionSort.sort (values);
 		
-		printValues(values,"After sort");
+//		printValues(values,"After sort");
+		System.out.println ("Time taken :" + ((System.currentTimeMillis () - startTime))+ " mili sec(s)");
+		
 	}
 
   private void printValues (Integer[] values,String message)
