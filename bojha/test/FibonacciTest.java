42c2ac22fc0c9b3b0e982b1f5b051be1256aae19
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-16 21:44:16 +0800

    Optimized code

--- bojha/test/FibonacciTest.java
+++ bojha/test/FibonacciTest.java
@@ -1,44 +1,41 @@
+import java.math.BigInteger;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FibonacciTest {
 
-	@Test
+	private static final int HIGHEST_TERM = 10000;
+
+  @Test
 	public void testGetFibonacci() {
 		// fail("Not yet implemented");
 		Fibonacci fibo = new Fibonacci();
-		int[] values = fibo.getFibonacci(30);
-		Assert.assertEquals(30, values.length);
-		Assert.assertTrue(values.length == 30);
+		long startTime = System.currentTimeMillis ();
+		
+		BigInteger[] values = fibo.getFibonacci(HIGHEST_TERM);
+		
+		Assert.assertEquals(HIGHEST_TERM, values.length);
+		Assert.assertTrue(values.length == HIGHEST_TERM);
+//		printArray(values);
+		System.out.println ("Time taken :" + (System.currentTimeMillis () - startTime)+ " ms");
 		try {
 			values = fibo.getFibonacci(-30);
 		} catch (IllegalArgumentException iae) {
 			Assert.assertTrue("Passing negative value should through IllegalArgumentException ",true);
 		}
+		
 		values = fibo.getFibonacci(0);
 
 		
 	}
 
-	@Test
-	public void testFibonacci() {
-		Fibonacci fibo = new Fibonacci();
-		Assert.assertEquals(0, fibo.fibonacci(0));
-		Assert.assertEquals(1, fibo.fibonacci(1));
-		Assert.assertEquals(1, fibo.fibonacci(2));
-		Assert.assertEquals(2, fibo.fibonacci(3));
-		Assert.assertEquals(3, fibo.fibonacci(4));
-		Assert.assertEquals(5, fibo.fibonacci(5));
-		Assert.assertEquals(8, fibo.fibonacci(6));
-		// System.out.println(fibo.fibonacci(50));
-		System.out.println(fibo.fibonacci(-1));
-		System.out.println(fibo.fibonacci(-2));
-		System.out.println(fibo.fibonacci(-3));
-		System.out.println(fibo.fibonacci(-4));
-		System.out.println(fibo.fibonacci(-5));
-		System.out.println(fibo.fibonacci(-6));
-		System.out.println(fibo.fibonacci(-7));
-		System.out.println(fibo.fibonacci(-8));
+	
+	
+	public void printArray(BigInteger[] values) {
+	  for (int i = 0; i < values.length; i++) {
+	    System.out.println (values[i]);
+	  }
 	}
 
 }
