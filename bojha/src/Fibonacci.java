b182b92690bedc7aabcc41a566d9e17cb29613b2
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-18 02:14:13 +0800

    nitial commit

--- bojha/src/Fibonacci.java
+++ bojha/src/Fibonacci.java
@@ -5,7 +5,7 @@
  */
 import java.math.BigInteger;
 
-<<<<<<< HEAD
+
 public class Fibonacci {
 
 	/**
@@ -34,47 +34,4 @@ public class Fibonacci {
 		}
 		return values;
 	}
-=======
-public class Fibonacci
-{
-
-  /**
-   * @param n
-   *          - number of fibonacci numbers like to get.
-   * @return n number of fibonacci numbers. e.g. if value of n = 3 it will
-   *         return an array of values 0,1,1 if value of n = 4 it will return an
-   *         array of values 0,1,1,2 if value of n = 5 it will return an array
-   *         of values 0,1,1,2,3
-   */
-  public BigInteger[] getFibonacci (int n)
-  {
-    // Create an array of size n.
-    BigInteger[] values = null;
-    if (n >= 0)
-    {
-      // array size can not be negative.
-      values = new BigInteger[n];
-      for (int i = 0; i < n; i++)
-      {
-        if (i == 1 || i == 2)
-        {
-          values[i] = BigInteger.ONE;
-        }
-        else
-          if (i == 0)
-          {
-            values[i] = BigInteger.ZERO;
-          }
-          else
-          {
-            values[i] = values[i - 1].add (values[i - 2]);
-          }
-      }
-    }
-    return values;
-  }
-
-
->>>>>>> 265d78d6883a7eadd0d44cb6a94bbec77440572a
-
 }
