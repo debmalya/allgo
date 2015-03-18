651104b0409bad9267ef23a3a66e350d2ccc8549
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-18 21:29:14 +0800

    Initial commit

---
+++ bojha/src/Company.java
@@ -0,0 +1,172 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+
+	// IMPORTANT: DO NOT MODIFY THIS CLASS
+	public static class Employee {
+
+		private final int id;
+
+		private final String name;
+
+		private List<Employee> reports;
+
+		public Employee(int id, String name) {
+			this.id = id;
+			this.name = name;
+			this.reports = new ArrayList<Employee>();
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public List<Employee> getReports() {
+			return reports;
+		}
+
+		public void addReport(Employee employee) {
+			reports.add(employee);
+		}
+	}
+
+	/*
+	 * Read the attached PDF for more explanation about the problem Note: Don't
+	 * modify the signature of this function
+	 * 
+	 * @param ceo
+	 * 
+	 * @param firstEmployee
+	 * 
+	 * @param secondEmployee
+	 * 
+	 * @return common manager for both employees that is closest to them.
+	 * 
+	 * Time complexity : Best Case : 1 (Directly reporting) 
+	 * Worst Case : O(V**2) V is the cardinality of the vertices.
+	 * Breadth first search is used here.
+	 * breadth of the tree.
+	 * 
+	 * Space complexity : This is tree type structure. CEO is the root. All
+	 * others are like branches. Not introduced any new data structure. Utilized existing data. No additional space.
+	 * 
+	 * Assumption : id is unique. It is used as Primary key.
+	 */
+	@SuppressWarnings("unused")
+	public static Employee closestCommonManager(Employee ceo,
+			Employee firstEmployee, Employee secondEmployee) {
+		// Implement me
+		Employee boss = getCommonBoss(ceo, firstEmployee, secondEmployee);
+		if (boss == null) {
+			boss = ceo;
+		}
+		return boss;
+	}
+
+	/**
+	 * Get nearest common boss.
+	 * @param ceo - CEO of the company.
+	 * @param firstEmployee - First employee
+	 * @param secondEmployee - Second employee
+	 * @return - Nearest common boss of the employees.
+	 */
+	private static Employee getCommonBoss(Employee ceo, Employee firstEmployee,
+			Employee secondEmployee) {
+		if (getBoss(ceo, firstEmployee).getId() == secondEmployee.getId()) {
+			return secondEmployee;
+		}
+		if (getBoss(ceo, secondEmployee).getId() == firstEmployee.getId()) {
+			return firstEmployee;
+		}
+		Employee firstEmployeeBoss = getBoss(ceo, firstEmployee);
+		Employee secondEmployeeBoss = getBoss(ceo, secondEmployee);
+
+		while (firstEmployeeBoss != null && secondEmployeeBoss != null
+				&& firstEmployeeBoss.getId() != secondEmployeeBoss.getId()) {
+			firstEmployeeBoss = getBoss(ceo, firstEmployeeBoss);
+			if (firstEmployeeBoss == null) {
+				return firstEmployeeBoss;
+			}
+			if (firstEmployeeBoss.getId() == secondEmployeeBoss.getId()) {
+				break;
+			}
+			secondEmployeeBoss = getBoss(ceo, secondEmployeeBoss);
+			if (secondEmployeeBoss == null) {
+				return null;
+			}
+
+		}
+		return firstEmployeeBoss;
+	}
+
+	/**
+	 * Get boss of the employee
+	 * @param ceo - CEO of the company
+	 * @param employee - any employee of the company.
+	 * @return Boss of the employee.
+	 */
+	private static Employee getBoss(Employee ceo, Employee employee) {
+		if (ceo.getReports().contains(employee)) {
+			return ceo;
+		} else {
+			List<Employee> employeeList = ceo.getReports();
+			int noOfEmployees = employeeList.size();
+			for (int i = 0; i < noOfEmployees; i++) {
+				Employee boss = getBoss(employeeList.get(i), employee);
+				if (boss != null) {
+					return boss;
+				}
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		Employee nina = new Company.Employee(1, "Nina");
+		Employee milton = new Company.Employee(2, "Milton");
+		Employee porter = new Company.Employee(3, "Porter");
+
+		Employee peter = new Company.Employee(4, "Peter");
+		peter.addReport(milton);
+		peter.addReport(nina);
+
+		Employee bob = new Company.Employee(5, "Bob");
+
+		Employee dom = new Company.Employee(6, "Dom");
+		dom.addReport(bob);
+		dom.addReport(peter);
+		dom.addReport(porter);
+
+		Employee samir = new Company.Employee(7, "Samir");
+		Employee michael = new Company.Employee(8, "Michael");
+		Employee ceo = new Company.Employee(9, "Bill");
+		ceo.addReport(dom);
+		ceo.addReport(samir);
+		ceo.addReport(michael);
+
+		Employee manager = Company.closestCommonManager(ceo, milton, nina);
+		// Peter
+		assert (manager.getName().equals("Peter"));
+
+		manager = Company.closestCommonManager(ceo, nina, porter);
+		// dom
+		assert (manager.getName().equals("Dom"));
+
+		manager = Company.closestCommonManager(ceo, nina, samir);
+		// bill
+		assert (manager.getName().equals("Bill"));
+
+		manager = Company.closestCommonManager(ceo, peter, nina);
+		// Peter
+		assert (manager.getName().equals("Peter"));
+
+		System.out.println("Game Over");
+
+	}
+
+};
