d8fe0ff6d07744717251dabf156fdf3deac5d225
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-18 15:14:30 +0800

    Initial Commit

---
+++ bojha/test/dj/string/StringUtilityTest.java
@@ -0,0 +1,75 @@
+package dj.string;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class StringUtilityTest
+{
+
+  @Test
+  public void testContainsUniqueCharacter ()
+  {
+    StringUtility utility = new StringUtility ();
+    assertTrue (utility.containsUniqueCharacter ("raju"));
+    assertFalse (utility.containsUniqueCharacter ("debmalya"));
+    assertFalse (utility.containsUniqueCharacter ("  "));
+    assertTrue (utility.containsUniqueCharacter ("abcdefghijklmnopqrstuvwxyz"));
+    assertFalse (utility.containsUniqueCharacter ("aA"));
+    assertFalse (utility.containsUniqueCharacter ("abcdefghijklmnopqrstuvwxyzA"));
+    try
+    {
+      utility.containsUniqueCharacter (null);
+      assertFalse ("Must throw exception", true);
+    }
+    catch (IllegalArgumentException iae)
+    {
+      assertTrue ("Must throw exception", true);
+    }
+  }
+
+  
+  @Test
+  public void testReverse() {
+    StringUtility utility = new StringUtility ();
+    String reversed = utility.reverse ("Debmalya");
+    assertTrue("aylambeD".equals (reversed));
+    reversed = utility.reverse ("Debmalya\n");
+    assertTrue("aylambeD".equals (reversed));
+    reversed = utility.reverse ("Raju");
+    assertTrue("ujaR".equals (reversed));
+  }
+  
+  @Test
+  public void testLowercase() {
+    StringUtility utility = new StringUtility ();
+    String lowered = utility.toLowerCase ("Debmalya");
+    assertTrue("debmalya".equals (lowered));
+    lowered = utility.toLowerCase ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    assertTrue("abcdefghijklmnopqrstuvwxyz".equals (lowered));
+    lowered = utility.toLowerCase ("A");
+    assertTrue("a".equals (lowered));
+  }
+  
+  @Test
+  public void testWithoutDuplicate() {
+    StringUtility utility = new StringUtility ();
+    
+    String modified = utility.removeDuplicate ("raju");
+    assertTrue("raju".equals (modified));
+    modified = utility.removeDuplicate ("Raju");
+    assertTrue("Raju".equals (modified));
+    modified = utility.removeDuplicate ("Debmalya");
+    assertTrue("Debmaly".equals (modified));
+  }
+  
+  @Test
+  public void testIsAnagram() {
+    StringUtility utility = new StringUtility ();
+    assertTrue(utility.isAnagram ("listen", "silent"));
+    assertFalse(utility.isAnagram ("listen", "silent1"));
+    assertFalse(utility.isAnagram ("listen", "silena"));
+  }
+  
+}
