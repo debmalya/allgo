e7a2f12371164a4962c7af6afcc830a4c945f92d
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-20 17:18:46 +0800

    Modified test case.

--- bojha/src/AllButOne.java
+++ bojha/src/AllButOne.java
@@ -1,7 +1,36 @@
+import java.util.Stack;
+
 
 public class AllButOne {
-	public int countRemainders(int S,int MOD) {
-		int r = -1;
-		return r;
+	public long countRemainders(int num) {
+		long value = Long.valueOf(toBinaryString(num));
+		int originalNum = num;
+		while (value % originalNum != 0) {
+			num++;
+			value = Long.valueOf(toBinaryString(num));
+			
+		}
+		return value;
 	}
+	
+	 /**
+	   * Convert to a binary string.
+	   * @param num
+	   * @return
+	   */
+	  private String toBinaryString(long num) {
+		  Stack<Long> values = new Stack<Long>();
+		  
+		  while (num > 1) {
+			  values.add(num % 2);
+			  num /= 2;
+		  }
+		  values.add(num);
+		  
+		  StringBuilder returnValue = new StringBuilder();
+		  while (!values.isEmpty()) {
+			  returnValue.append(values.pop());
+		  }
+		  return returnValue.toString();
+	  }
 }
