088e2730ff42c98a4ccd46dbd3030d3a6052a640
Debmalya Jash <debmalya.jash@gmail.com>
2015-03-22 10:18:55 +0800

    Make modification to get the highest palindrome

--- bojha/src/MathUtil.java
+++ bojha/src/MathUtil.java
@@ -1,120 +1,134 @@
 import java.util.Stack;
 
+public class MathUtil {
 
-public class MathUtil
-{
+	public MathUtil() {
+		// TODO Auto-generated constructor stub
+	}
 
-  public MathUtil ()
-  {
-    // TODO Auto-generated constructor stub
-  }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
 
+	public static long getSmallestMultiple(long term) {
+		long result = 1;
+		for (int i = 2; i <= term; i++) {
+			result = lcd(result, i);
+		}
+		return result;
+	}
 
-  /**
-   * @param args
-   */
-  public static void main (String[] args)
-  {
-    // TODO Auto-generated method stub
-    
-  }
+	public static long lcd(long a, long b) {
+		return a * b / gcd(a, b);
+	}
 
-
-  public static long getSmallestMultiple(long term) {
-    long result  = 1;
-    for (int i =2; i <=term; i++) {
-      result = lcd(result,i);
-    }
-    return result;
-  }
-  
-  
-  public static long lcd(long a,long b) {
-    return a * b / gcd(a,b);
-  }
-
-  public static long gcd (long a, long b)
-  {
-	  long max = Math.max(a, b);
+	public static long gcd(long a, long b) {
+		long max = Math.max(a, b);
 		if (a == 0 || b == 0 || a == b) {
 			return max;
-		}  else {			
+		} else {
 			long min = Math.min(a, b);
 			if (max % min == 0) {
 				return min;
 			}
-			
-			return gcd(max,max % min);
+
+			return gcd(max, max % min);
 		}
-  }
-  
-  public int getNumberOfFipsRequired(int number1,int number2) {
-    String value = Integer.toBinaryString (number1 ^ number2);
-    int count = 0;
-    for (int i = 0; i < value.length (); i++) {
-      if (value.charAt (i) == '1') {
-        count++;
-      }
-    }
-    return count;
-  }
-  
-  public long getHighestPalindrome(int numLength) {
-	  long result = 1L;
-	  long number = 0L;
-	  int count = 0;
-	  
-	  while (count < numLength) {
-		  number *=10;
-		  number += 9;
-		  count++;
-	  }
-	  
-	  long multiplier = number;
-	  while (true) {
-		  result = multiplier*number;
-		  if (isPalindrom(String.valueOf(result))) {
-			  break;
-		  }
-		  number--;
-	  }
-	  
-	  return result;
-  }
-  
-  /**
-   * To check whether the string is palindromic  or not.
-   * 
-   * @param str to be checked.
-   * @return true if palindrom, false otherwise.
-   */
-  public boolean isPalindrom(String str) {
-    int len = str.length ();
-    for (int i = 0; i < len /2 ; i++)
-      if (str.charAt (i)!= str.charAt (len -i -1)) {
-        return false;
-      }
-    return true;
-  }
-  /**
-   * Convert to a binary string.
-   * @param num
-   * @return
-   */
-  public String toBinaryString(long num) {
-	  Stack<Long> values = new Stack<Long>();
-	  
-	  while (num > 1) {
-		  values.add(num % 2);
-		  num /= 2;
-	  }
-	  values.add(num);
-	  
-	  StringBuilder returnValue = new StringBuilder();
-	  while (!values.isEmpty()) {
-		  returnValue.append(values.pop());
-	  }
-	  return returnValue.toString();
-  }
-} 
+	}
+
+	public int getNumberOfFipsRequired(int number1, int number2) {
+		String value = Integer.toBinaryString(number1 ^ number2);
+		int count = 0;
+		for (int i = 0; i < value.length(); i++) {
+			if (value.charAt(i) == '1') {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	/**
+	 * A palindromic number reads the same both ways. The largest palindrome
+	 * made from the product of two 2-digit numbers is 9009 = 91 × 99.
+	 * 
+	 * Find the largest palindrome made from the product of two 3-digit numbers.
+	 * 
+	 * @param numLength - digits in the multiplier (2,3,....)
+	 * @return highest palindrome
+	 */
+	public long getHighestPalindrome(int numLength) {
+		long result = 1L;
+		long number = 0L;
+		int count = 0;
+		int lowerLimit = 0;
+
+		while (count < numLength) {
+			number *= 10;
+			number += 9;
+			count++;
+			
+			
+		}
+		lowerLimit = (int)(number / 10) + 1;
+//		System.out.println(" number " + number+" lowerLimit " + lowerLimit);
+		long multiplier = number;
+		while (true) {
+			result = multiplier * number;
+			if (isPalindrom(String.valueOf(result))) {
+				System.out.println(number+ " X " + multiplier);
+				break;
+			}
+			
+			number--;
+			if (number < lowerLimit) {
+				number = --multiplier;	
+				System.out.println(number+ " Y " + multiplier);
+			}
+			
+		}
+
+		return result;
+	}
+
+	/**
+	 * To check whether the string is palindromic or not.
+	 * 
+	 * @param str
+	 *            to be checked.
+	 * @return true if palindrom, false otherwise.
+	 */
+	public boolean isPalindrom(String str) {
+		int len = str.length();
+		for (int i = 0; i < len / 2; i++)
+			if (str.charAt(i) != str.charAt(len - i - 1)) {
+				return false;
+			}
+		return true;
+	}
+
+	/**
+	 * Convert to a binary string.
+	 * 
+	 * @param num
+	 * @return
+	 */
+	public String toBinaryString(long num) {
+		Stack<Long> values = new Stack<Long>();
+
+		while (num > 1) {
+			values.add(num % 2);
+			num /= 2;
+		}
+		values.add(num);
+
+		StringBuilder returnValue = new StringBuilder();
+		while (!values.isEmpty()) {
+			returnValue.append(values.pop());
+		}
+		return returnValue.toString();
+	}
+}
--- bojha/test/MathUtilTest.java
+++ bojha/test/MathUtilTest.java
@@ -3,50 +3,70 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
 
+public class MathUtilTest {
 
+	@Test
+	public void testGcd() {
 
-public class MathUtilTest
-{
+		assertEquals(2, MathUtil.gcd(2, 6));
+		assertEquals(2, MathUtil.gcd(2, 0));
+		assertEquals(2, MathUtil.gcd(0, 2));
+		assertEquals(0, MathUtil.gcd(0, 0));
+		assertEquals(1, MathUtil.gcd(4, 3));
+		assertEquals(1, MathUtil.gcd(3, 4));
+	}
 
-  @Test
-  public void testGcd ()
-  {
+	@Test
+	public void testLcd() {
 
-    assertEquals (2, MathUtil.gcd (2, 6));
-    assertEquals (2, MathUtil.gcd (2, 0));
-    assertEquals (2, MathUtil.gcd (0, 2));
-    assertEquals (0, MathUtil.gcd (0, 0));
-    assertEquals (1, MathUtil.gcd (4, 3));
-    assertEquals (1, MathUtil.gcd (3, 4));
-  }
+		assertEquals(6, MathUtil.lcd(2, 6));
+		assertEquals(0, MathUtil.lcd(2, 0));
+		assertEquals(12, MathUtil.lcd(4, 3));
+		assertEquals(12, MathUtil.lcd(3, 4));
+	}
 
+	@Test
+	public void testGetBinary() {
+		MathUtil util = new MathUtil();
+		Assert.assertTrue("1".equals(util.toBinaryString(1)));
+		Assert.assertTrue("10".equals(util.toBinaryString(2)));
+		Assert.assertTrue("11".equals(util.toBinaryString(3)));
+		Assert.assertTrue("100".equals(util.toBinaryString(4)));
+		Assert.assertTrue("101".equals(util.toBinaryString(5)));
+		Assert.assertTrue("110".equals(util.toBinaryString(6)));
+		Assert.assertTrue("111".equals(util.toBinaryString(7)));
+	}
 
-  
-  @Test
-  public void testLcd ()
-  {
+	@Test
+	public void testHighestPalindrome() {
+		MathUtil util = new MathUtil();
 
-    assertEquals (6, MathUtil.lcd (2, 6));
-    assertEquals (0, MathUtil.lcd (2, 0));
-    assertEquals (12, MathUtil.lcd (4, 3));
-    assertEquals (12, MathUtil.lcd (3, 4));
-  }
+		assertEquals(9009, util.getHighestPalindrome(2));
+		System.out.println(util.getHighestPalindrome(3));
+	}
 
+	@Test
+	public void testPalindrome() {
+		MathUtil util = new MathUtil();
+		long value = 99999L;
 
+		while (true) {
+			int divisor = 100;
+			while (divisor < 1000) {
+				
+				if (util.isPalindrom(String.valueOf(value))) {
+					System.out.println(value);
+					long dividend = value / divisor;
+					if (value % divisor == 0 && dividend > 100) {
+						System.out.println("Looking for :" + value+ " dividend " + dividend + " divisor "+divisor);
+						return;
+					}
+					divisor++;
+				}
+				value--;
+			}
+		}
 
-
-  
-  @Test
-  public void testGetBinary() {
-	  MathUtil util = new MathUtil();
-	  Assert.assertTrue("1".equals(util.toBinaryString(1)));
-	  Assert.assertTrue("10".equals(util.toBinaryString(2)));
-	  Assert.assertTrue("11".equals(util.toBinaryString(3)));
-	  Assert.assertTrue("100".equals(util.toBinaryString(4)));
-	  Assert.assertTrue("101".equals(util.toBinaryString(5)));
-	  Assert.assertTrue("110".equals(util.toBinaryString(6)));
-	  Assert.assertTrue("111".equals(util.toBinaryString(7)));
-  }
-  
+	}
 
 }
