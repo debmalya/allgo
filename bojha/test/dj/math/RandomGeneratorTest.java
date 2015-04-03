5662195d6a91390707024c85ab7ca99238f639ee
Debmalya Jash <debmalya.jash@gmail.com>
2015-04-03 09:28:31 +0800

    Modified RandomGeneratorTest to print number of times minRange occurs and number of time maxRange occurs.

--- bojha/test/dj/math/RandomGeneratorTest.java
+++ bojha/test/dj/math/RandomGeneratorTest.java
@@ -1,7 +1,6 @@
 package dj.math;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 
@@ -11,10 +10,21 @@ public class RandomGeneratorTest
   @Test
   public void test ()
   {
+	long minCount = 0;
+	long maxCount = 0;
+	long minRange = 200L;
+	long maxRange = 401L;
     for (int i = 0; i < 1000000; i++) {
-      long l= RandomGenerator.randomWithinRange (200L, 400L);
+      long l= RandomGenerator.randomWithinRange (minRange, maxRange);
       Assert.assertTrue("Number must be >= 200 && <= 400 " + l,l >= 200L && l <= 400L);
+      if (l == minRange) {
+    	  minCount++;
+      } else if (l == maxRange - 1) {
+    	  maxCount++;
+      }
     }
+    System.out.println(minRange+" occurs " + minCount);
+    System.out.println((maxRange - 1)+" occurs " + maxCount);
   }
 
 }
