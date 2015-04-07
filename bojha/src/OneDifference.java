37ea2da1ca9b9ee6d117c8158f61e8e4facaa7a1
Debmalya Jash <debmalya.jash@gmail.com>
2015-04-08 07:10:53 +0800

    Looking for better way

--- bojha/src/OneDifference.java
+++ bojha/src/OneDifference.java
@@ -3,12 +3,26 @@
  * An edit can be a modification, addition or removal of a single character.
  */
 public class OneDifference {
-	public boolean fn(String left, String right) {
-		
+	public boolean fn1(String left, String right) {
+
 		if (Math.abs(left.length() - right.length()) > 1) {
 			return false;
 		}
+
+		if (left.length() > right.length()) {
+			return left.replace(right, "").trim().length() == 1;
+		} else {
+			return right.replace(left, "").trim().length() == 1;
+		}
 		
+	}
+
+	public boolean fn(String left, String right) {
+
+		if (Math.abs(left.length() - right.length()) > 1) {
+			return false;
+		}
+
 		left = left.trim();
 		right = right.trim();
 
@@ -23,4 +37,5 @@ public class OneDifference {
 		}
 		return true;
 	}
+
 }
