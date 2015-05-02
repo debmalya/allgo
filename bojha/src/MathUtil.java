3391483470ef436a7185ce7e4cde162c6a1a4ca6
Debmalya Jash <debmalya.jash@gmail.com>
2015-05-02 19:15:13 +0800

    Added method getIndex

--- bojha/src/MathUtil.java
+++ bojha/src/MathUtil.java
@@ -249,9 +249,11 @@ public class MathUtil {
 		int[][] sum = null;
 		if (arr != null ) {
 			int maxLen = arr.length - 1;
+			
 			sum = new int[2][];
 			sum[0] = new int[arr.length];
 			sum[1] = new int[arr.length];
+			
 			for (int i = 0; i < arr.length; i++) {
 				sum[0][i] += arr[i];
 				sum[1][maxLen - i] += arr[maxLen - i];
@@ -263,5 +265,102 @@ public class MathUtil {
 		}
 		return sum;
 		
+	}
+	
+	/**
+	 * Returns prefix sum and suffix sum.
+	 * @param value string containing numbers each number is separated by space.
+	 * @return prefix and suffix sum
+	 * if int arr is 1 2 3 4
+	 * it will return first element as prefix
+	 * 1 1+2 1+2+3 1+2+3+4
+	 * 1 3 6 10
+	 * 
+	 * second element as suffix sum
+	 * 1+2+3+4 2+3+4 3+4 4
+	 * 10 9 7 4
+	 */
+	public static int[][] getSum(String value){
+		int[][] sum = null;
+		String[] arr = value.split(" ");
+		if (arr != null ) {
+			int maxLen = arr.length - 1;
+			
+			sum = new int[2][];
+			sum[0] = new int[arr.length];
+			sum[1] = new int[arr.length];
+			
+			for (int i = 0; i < arr.length; i++) {
+				sum[0][i] += Integer.parseInt(arr[i]);
+				sum[1][maxLen - i] += Integer.parseInt(arr[maxLen - i]);
+				if (i < maxLen) {
+					sum[0][i + 1] += sum[0][i]; 
+					sum[1][maxLen - i - 1] += sum[1][maxLen - i];
+				}
+			}
+		}
+		return sum;
+		
+	}
+	/**
+	 * 
+	 * @param arr integer array
+	 * @return index where sum of earlier indexes are equal to sum of later indexes.
+	 * 
+	 * e.g. for the input 2 3 1 6
+	 * at index 2, sum of earlier indexes 2+3+1 = 6
+	 * here we have to return 2.
+	 * 
+	 * If there is no such index return 0.
+	 * 
+	 */
+	public static int getMatchingSum(int[] arr) {
+		
+		int[][] sum = getSum(arr);
+		
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (sum[0][i] == sum[1][i + 1]) {
+				return i;
+			} else {
+				for (int j = 0; j < i; j++) {
+					if ((sum[0][i] - sum[0][j])== sum[1][i + 1]) {
+						return i;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param arr String containing integer, each integer is separated by space.
+	 * @return index where sum of earlier indexes are equal to sum of later indexes.
+	 * 
+	 * e.g. for the input 2 3 1 6
+	 * at index 2, sum of earlier indexes 2+3+1 = 6
+	 * here we have to return 2.
+	 * 
+	 * If there is no such index return 0.
+	 * 
+	 */
+	public static int getMatchingSum(String arr) {
+		
+		int[][] sum = getSum(arr);
+		
+		
+		for (int i = 0; i < sum[0].length - 1; i++) {
+			if (sum[0][i] == sum[1][i + 1]) {
+				return i;
+			} else {
+				for (int j = 0; j < i; j++) {
+					if ((sum[0][i] - sum[0][j])== sum[1][i + 1]) {
+						return i;
+					}
+				}
+			}
+		}
+		return 0;
 	}
 }
