88e6ed5d601f3d8baf9d1bf483bc3099e0b8d65e
Debmalya Jash <debmalya.jash@gmail.com>
2015-05-05 21:54:07 +0800

    Added method callWebAPI copied from NEA developer guide.

--- bojha/src/dj/nea/NEAInfoParser.java
+++ bojha/src/dj/nea/NEAInfoParser.java
@@ -3,6 +3,12 @@
  */
 package dj.nea;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * @author debmalyajash
  *
@@ -13,12 +19,67 @@ public class NEAInfoParser {
 	 * KEY_REF to call nea api
 	 */
 	private static final String KEY_REF = "<<Your API KEY>>";
+
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] datasets = new String[]{"nowcast"};
+		for (String eachData:datasets) {
+			try {
+				callNEAWebAPI(eachData, KEY_REF);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 
 	}
 
+	private static void callNEAWebAPI(String datasetName, String keyRef)
+			throws Exception {
+		// Step 1: Construct URL
+		String url = "http://www.nea.gov.sg/api/WebAPI?dataset=" + datasetName
+				+ "&keyref=" + KEY_REF;
+		// Step 2: Call API Url
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		// Step 3: Check the response status
+		if (responseCode == 200) {
+			// Step 3a: If response status == 200
+			// print the received xml
+			 System.out.println(readStream(con.getInputStream()));
+		} else {
+			// Step 3b: If response status != 200
+			// print the error received from server
+			// System.out.println("Error in accessing API - " +
+			readStream(con.getErrorStream());
+		}
+	}
+
+	// Read the responded result
+	private static String readStream(InputStream inputStream) throws Exception {
+		BufferedReader reader = null;
+		StringBuffer response = new StringBuffer();
+		try {
+			reader = new BufferedReader(new InputStreamReader(inputStream));
+			String inputLine;
+			
+			while ((inputLine = reader.readLine()) != null) {
+				response.append(inputLine);
+			}
+		} finally {
+			if (reader != null) {
+				reader.close();
+			}
+		}
+		return response.toString();
+	}
 }
