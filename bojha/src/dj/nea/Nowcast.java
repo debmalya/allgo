59379113f4e898402c4f0fed9ca5610b10a53a07
Debmalya Jash <debmalya.jash@gmail.com>
2015-05-08 07:09:37 +0800

    Initial Commit

---
+++ bojha/src/dj/nea/Nowcast.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package dj.nea;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class Nowcast {
+	private String forecast;
+	private String area;
+	private String latitude;
+	private String longitude;
+	/**
+	 * @return the forecast
+	 */
+	public String getForecast() {
+		return forecast;
+	}
+	/**
+	 * @param forecast the forecast to set
+	 */
+	public void setForecast(String forecast) {
+		this.forecast = forecast;
+	}
+	/**
+	 * @return the area
+	 */
+	public String getArea() {
+		return area;
+	}
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(String area) {
+		this.area = area;
+	}
+	/**
+	 * @return the latitude
+	 */
+	public String getLatitude() {
+		return latitude;
+	}
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	/**
+	 * @return the longitude
+	 */
+	public String getLongitude() {
+		return longitude;
+	}
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+}
