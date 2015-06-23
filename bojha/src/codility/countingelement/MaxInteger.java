04cda56abf033765600b777f23302aae27c8dc7e
Debmalya Jash <debmalya.jash@gmail.com>
2015-06-24 07:20:42 +0800

    Initial commit

---
+++ bojha/src/codility/countingelement/MaxInteger.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package codility.countingelement;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class MaxInteger {
+	/**
+	 * You are given N counters, initially set to 0, and you have two possible
+	 * operations on them:
+	 * 
+	 * increase(X) − counter X is increased by 1, max counter − all counters are
+	 * set to the maximum value of any counter. A non-empty zero-indexed array A
+	 * of M integers is given. This array represents consecutive operations:
+	 * 
+	 * if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X), if
+	 * A[K] = N + 1 then operation K is max counter. For example, given integer
+	 * N = 5 and array A such that:
+	 * 
+	 * A[0] = 3 A[1] = 4 A[2] = 4 A[3] = 6 A[4] = 1 A[5] = 4 A[6] = 4 the values
+	 * of the counters after each consecutive operation will be:
+	 * 
+	 * (0, 0, 1, 0, 0) (0, 0, 1, 1, 0) (0, 0, 1, 2, 0) (2, 2, 2, 2, 2) (3, 2, 2,
+	 * 2, 2) (3, 2, 2, 3, 2) (3, 2, 2, 4, 2) The goal is to calculate the value
+	 * of every counter after all operations.
+	 * 
+	 * Assume that the following declarations are given:
+	 * 
+	 * struct Results { int * C; int L; };
+	 * 
+	 * Write a function:
+	 * 
+	 * struct Results solution(int N, int A[], int M);
+	 * 
+	 * that, given an integer N and a non-empty zero-indexed array A consisting
+	 * of M integers, returns a sequence of integers representing the values of
+	 * the counters.
+	 * 
+	 * The sequence should be returned as:
+	 * 
+	 * a structure Results (in C), or a vector of integers (in C++), or a record
+	 * Results (in Pascal), or an array of integers (in any other programming
+	 * language). For example, given:
+	 * 
+	 * A[0] = 3 A[1] = 4 A[2] = 4 A[3] = 6 A[4] = 1 A[5] = 4 A[6] = 4 the
+	 * function should return [3, 2, 2, 4, 2], as explained above.
+	 * 
+	 * Assume that:
+	 * 
+	 * N and M are integers within the range [1..100,000]; each element of array
+	 * A is an integer within the range [1..N + 1]. Complexity:
+	 * 
+	 * expected worst-case time complexity is O(N+M); expected worst-case space
+	 * complexity is O(N), beyond input storage (not counting the storage
+	 * required for input arguments). Elements of input arrays can be modified.
+	 * 
+	 * Score : 77
+	 */
+	public int[] solution(int N, int[] A) {
+		int[] r = new int[N];
+		
+		boolean[] setMax = new boolean[N];
+		int prevSetMax = 0;
+		int currentSetMax = 0;
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] == N + 1) {
+				
+				setMax = new boolean[N];
+				prevSetMax = currentSetMax;
+			} else {
+				if (!setMax[A[i] - 1] && prevSetMax != 0) {
+					r[A[i] - 1] = prevSetMax;
+					setMax[A[i] - 1] = true;
+				}
+				r[A[i] - 1]++;
+				currentSetMax = Math.max(currentSetMax, r[A[i] - 1]);
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (!setMax[i]) {
+				r[i] = prevSetMax;
+			}
+		}
+		return r;
+	}
+}
