b2f49ba7c3a132487661122900c3b6b10b8baa3d
Debmalya Jash <debmalya.jash@gmail.com>
2015-07-24 00:08:06 +0800

    Added new method.

--- bojha/src/CodeFights.java
+++ bojha/src/CodeFights.java
@@ -469,4 +469,34 @@ public class CodeFights {
 			return false;
 		}
 	}
+
+	/**
+	 * Given an array of positive integers and an integer sum, find the maximum
+	 * number of elements in the given array that sum up to sum.
+	 * 
+	 * 
+	 * max_num_of_ele(10, [10, 3, 7]) = 2 max_num_of_ele(3, [1, 1, 2, 1 ]) = 3
+	 * 
+	 * Input 1 (sum) → integer : A None negative integer
+	 * 
+	 * Input 2 (list) → array.integer : Array of Positive integers
+	 * 
+	 * Output → integer : Maximum number of elements that sum up to sum. Return
+	 * 0 if it can't be done.
+	 */
+	public int max_num_of_ele(int sum, int[] list) {
+		Arrays.sort(list);
+		int s = 0;
+		int c = 0;
+		for (int i = 0; i < list.length && s < sum; i++){
+			s += list[i];
+			c++;
+			if (s == sum) {
+				return c;
+			}
+		}
+		return 0;
+	}
+
+
 }
