c58276f8de5aa0fd2af82545403f0be754df7e03
Debmalya Jash <debmalya.jash@gmail.com>
2015-07-28 07:17:36 +0800

    Initial Commit

---
+++ bojha/src/lambda/sample/Lesson2Mine.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package lambda.sample;
+
+import java.util.List;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class Lesson2Mine {
+
+	public void exercise1(List<String> originalList){
+		originalList.replaceAll(String::toLowerCase);
+		originalList.forEach(System.out::println);
+		int $cgh;
+	}
+}
