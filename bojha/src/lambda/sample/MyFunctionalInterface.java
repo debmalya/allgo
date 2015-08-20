c94bf1a84ff9c58d81d6925c5b29a1b966ded50e
Debmalya Jash <debmalya.jash@gmail.com>
2015-08-21 07:08:26 +0800

    Initial commit

---
+++ bojha/src/lambda/sample/MyFunctionalInterface.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package lambda.sample;
+
+/**
+ * @author debmalyajash
+ *
+ */
+@FunctionalInterface
+public interface MyFunctionalInterface {
+	public abstract String say(String listen);
+}
