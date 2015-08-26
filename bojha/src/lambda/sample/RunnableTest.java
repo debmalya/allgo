c39a80765934c3e9e1f3df5c7fcba2323c276e24
Debmalya Jash <debmalya.jash@gmail.com>
2015-08-27 07:14:55 +0800

    Initial commit

---
+++ bojha/src/lambda/sample/RunnableTest.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package lambda.sample;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class RunnableTest {
+	public static void main(String[] args) {
+		      
+		      System.out.println("=== RunnableTest ===");
+		     
+		     // Anonymous Runnable
+		     Runnable r1 = new Runnable(){
+		       
+		       @Override
+		       public void run(){
+		         System.out.println("Hello world one!");
+		       }
+		     };
+		     
+		     // Lambda Runnable
+		     Runnable r2 = () -> System.out.println("Hello world two!");
+		     
+		     // Run em!
+		     r1.run();
+		     r2.run();
+		     
+		   }
+}
