fd8608faca5515125ec6808589cb2feab14bb211
Debmalya Jash <debmalya.jash@gmail.com>
2015-11-18 05:52:13 +0800

    Initial commit

---
+++ bojha/src/amazed/BinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package amazed;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class BinaryTree {
+
+	/**
+	 * @param args
+	 *            Given two Binary Trees (not BST). Each node of both trees has
+	 *            an integer value. Validate whether both trees have the same
+	 *            integers, there could be repetitive integers. ex- Tree1: 5 1 6
+	 *            5 4 3 6
+	 * 
+	 *            Tree2: 1 3 4 6 5 have identical integers.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
