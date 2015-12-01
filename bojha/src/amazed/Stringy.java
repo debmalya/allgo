2cfc55de206427c72163317a299fae0a8663ccd8
Debmalya Jash <debmalya.jash@gmail.com>
2015-12-01 22:37:08 +0800

    Initial commit

---
+++ bojha/src/amazed/Stringy.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package amazed;
+
+/**
+ * @author debmalyajash
+ *
+ */
+public class Stringy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int value = Integer.parseInt("C1", 16);
+		int commandId = value ^ 192;
+		System.out.println(commandId);
+
+	}
+
+	/**
+	 * Given a string and two words which are present in the string, find the
+	 * minimum distance between the words Eg: "the brown qucik frog quick the",
+	 * "the" "quick" O/P -> 1 "the quick the brown quick brown the frog", "the"
+	 * "the" O/P -> 2
+	 * 
+	 * @param sentence
+	 * @param word1
+	 * @param word2
+	 * @return
+	 */
+	int minimumDistance(String sentence, String word1, String word2) {
+		return 0;
+	}
+	
+	
+
+}
